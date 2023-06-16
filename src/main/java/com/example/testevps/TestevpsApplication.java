991d35df0e67b3fb574feb3e9458672d25eb83a0
enzojpn <enzo_jpn@hotmail.com>
2023-06-15 21:37:26 -0300

    first commit

---
+++ src/main/java/com/example/testevps/TestevpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testevps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestevpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestevpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testevps/TestevpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testevps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestevpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
